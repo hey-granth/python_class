4b01d7ab5b151394d3e55f7c60204860f05099c5
Granth Agarwal <heygranth@gmail.com>
2024-12-04 09:53:53 +0530

    updated numpy-arrays.py

--- numpy-arrays.py
+++ numpy-arrays.py
@@ -1,3 +1,15 @@
 import numpy
+import numpy as np
+
+# Create a NumPy 0-D array
+arr = np.array(100)
+print(arr)
+
+# Create a NumPy 1-D array
 Myarray = numpy.array([1, 2, 3, 4, 5])
 print(Myarray)
+
+# Create a NumPy multidimensional array
+Myarr = np.array([[1, 2, 3], [4, 5, 6]])
+print(Myarr)
+Myarr.ndim
